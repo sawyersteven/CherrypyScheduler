6294cf65ecd1551cb938573ff6a20e1ecd02498b
sawyersteven <sawyerstevenk@gmail.com>
2018-07-04 22:31:04 -0400

    Adjust tests for coverage and to ensure state of server

--- tests/tests.py
+++ tests/tests.py
@@ -6,6 +6,7 @@ sys.path.insert(0, os.path.join(os.getcwd(), '../'))
 import unittest                                 # noqa
 import cherrypy                                 # noqa
 import threading                                # noqa
+import urllib.request                           # noqa
 import time                                     # noqa
 from datetime import datetime, timedelta        # noqa
 from cherrypyscheduler import SchedulerPlugin   # noqa
@@ -30,7 +31,12 @@ class TestScheduler(unittest.TestCase):
         TestScheduler._test_last_execution = datetime.now()
         TestScheduler.task_counter += 1
 
-    def test_00_create_task(self):
+    def test_00_server_online(self):
+        ''' Tests that cherrypy server is running and properly configured '''
+        r = urllib.request.urlopen('http://localhost:8080')
+        self.assertEqual(r.fp.read(), b'Test server running')
+
+    def test_01_create_task(self):
         ''' Tests creation of a simple task and that is is properly
             placed in SchedulerPlugin.task_list
 
@@ -39,7 +45,7 @@ class TestScheduler(unittest.TestCase):
         TestScheduler.task_instance = SchedulerPlugin.ScheduledTask(self.now.hour, self.now.minute + 1, 30, self.task, auto_start=False, name=self.task_name)
         self.assertIsInstance(SchedulerPlugin.task_list[self.task_name], SchedulerPlugin._SchedulerPlugin__ScheduledTask)
 
-    def test_01_next_execution(self):
+    def test_02_next_execution(self):
         ''' Tests that task.next_execution is set correctly
             for created but not started task, and started task
         '''
@@ -47,20 +53,20 @@ class TestScheduler(unittest.TestCase):
         self.task_instance.start()
         self.assertEqual(self.task_instance.next_execution, self.now + timedelta(seconds=30))
 
-    def test_02_task_call(self):
+    def test_03_task_call(self):
         ''' Tests that task is called (30s) '''
         start_count = self.task_counter
         time.sleep(32)
         self.assertEqual(start_count + 1, self.task_counter)
 
-    def test_03_record_json(self):
+    def test_04_record_json(self):
         ''' Tests that the persistence record is written correctly
             By default this tests both read and write methods of _Record
         '''
         le = SchedulerPlugin.record_handler.read()[self.task_name]['last_execution']
         self.assertEqual(le, self.task_instance.last_execution)
 
-    def test_04_stop(self):
+    def test_05_stop(self):
         ''' Tests that the task can be stopped and Thread timer is cancelled (30s)
             Since Timer objects have no properties that indicate remaining time or
             status we will check the task_counter, wait for a timer iteration, and
@@ -71,14 +77,14 @@ class TestScheduler(unittest.TestCase):
         time.sleep(32)
         self.assertEqual(start_count, self.task_counter)
 
-    def test_05_restart(self):
+    def test_06_restart(self):
         ''' Tests restarting scheduler with intial args '''
         a_s = self.task_instance.auto_start
         self.task_instance.restart()
         self.assertEqual(a_s, self.task_instance.auto_start)
         self.assertEqual(self.task_instance.next_execution, None)
 
-    def test_06_reload(self):
+    def test_07_reload(self):
         ''' Tests reloading scheduler with 45s delay '''
         now = datetime.now()
         start_count = self.task_counter
@@ -88,13 +94,13 @@ class TestScheduler(unittest.TestCase):
         time.sleep(15)
         self.assertEqual(start_count + 1, self.task_counter)
 
-    def test_07_now(self):
+    def test_08_now(self):
         ''' Test bypassing timer '''
         start_count = self.task_counter
         self.task_instance.now()
         self.assertEqual(start_count + 1, self.task_counter)
 
-    def test_08_creep(self):
+    def test_09_creep(self):
         ''' Tests time consistency
             This plugin attempts to only be accurate to +- one second,
             so we will test if repeated task calls creep upward in interval.
